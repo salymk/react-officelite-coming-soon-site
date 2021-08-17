e80770002d29366fc6b1866af395df69589003f2
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-08-16 21:08:51 -0600

    feat: create html structure with content

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/shared/logo.svg';
+
+const Header = () => (
+  <header>
+    <img src={logo} alt="Blue circles with officelite text" />
+  </header>
+);
+
+export default Header;
