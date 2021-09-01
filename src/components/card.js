b16eed7e7cf3e6862403dafb0248d903275fd3fa
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-01 16:45:14 -0600

    refactor: add card header to hold tier, price, users and also button
    - show btn--tablet on tablet screens and hide btn--mobile on tablet screens

--- src/components/card.js
+++ src/components/card.js
@@ -5,17 +5,20 @@ import Button from './button';
 
 const Card = (props) => (
   <div className={`card ${props.cardClassName}`}>
-    <p className="card__tier">{props.tier}</p>
-    <div className="card__price">
-      <h2>{props.price}</h2>
-      <p>{props.users}</p>
+    <div className="card__header">
+      <p className="card__tier">{props.tier}</p>
+      <div className="card__price">
+        <h2>{props.price}</h2>
+        <p>{props.users}</p>
+      </div>
+      <Button btnClass="btn--tablet" description={props.cta} />
     </div>
     <div className="card__features">
       {props.features?.map((feature) => (
         <p key={feature}>{feature}</p>
       ))}
     </div>
-    <Button description={props.cta} />
+    <Button btnClass="btn--mobile" description={props.cta} />
   </div>
 );
 
