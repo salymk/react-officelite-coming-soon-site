bb8faa81091132244b73b405619276114602202e
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-06 17:20:23 -0600

    refactor: rewrite all of the html structure

--- src/Signup.js
+++ src/Signup.js
@@ -8,30 +8,23 @@ import './scss/main.scss';
 
 const Signup = () => (
   <>
-    {/* <Header /> */}
-
     <main className="signup">
-      <section className="signup-container">
+      <div className="signup-hero">
         <Header />
-        <section className="hero">
-          <div className="container grid">
-            <div className="hero__content">
-              <h1>Work smarter. Save time.</h1>
-              <p>
-                Easily manage your projects. Get on the list and receive in-app
-                perks available only to early subscribers. We are moving into
-                final development and getting ready for official launch soon.
-              </p>
-              <Countdown className="countdown--light" />
-            </div>
-            <div className="form-container">
-              <Form />
-            </div>
+        <div className="container flex">
+          <div className="signup-hero__content">
+            <h1>Work smarter. Save time.</h1>
+            <p>
+              Easily manage your projects. Get on the list and receive in-app
+              perks available only to early subscribers. We are moving into
+              final development and getting ready for official launch soon.
+            </p>
+            <Countdown className="countdown--light" />
           </div>
-        </section>
-      </section>
-
-      <section className="bg-dark" />
+          <Form />
+        </div>
+      </div>
+      <div className="bg-dark" />
     </main>
   </>
 );
