245d313f656b87eb71a865124975f6ad03ebcd7f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-06 11:34:58 -0600

    feat: create signup page

--- src/Signup.js
+++ src/Signup.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from './components/button';
+import Header from './components/header';
+import Countdown from './components/countdown';
+import Form from './components/form';
+import './scss/main.scss';
 
 const Signup = () => (
   <>
-    <h1> Work smarter. Save time.</h1>
-    <Link to="/">
-      <Button btnClass="btn--primary" description="Get started" />
-    </Link>
+    {/* <Header /> */}
+
+    <main className="signup">
+      <section className="signup-container">
+        <Header />
+        <section className="hero">
+          <div className="container grid">
+            <div className="hero__content">
+              <h1>Work smarter. Save time.</h1>
+              <p>
+                Easily manage your projects. Get on the list and receive in-app
+                perks available only to early subscribers. We are moving into
+                final development and getting ready for official launch soon.
+              </p>
+              <Countdown className="countdown--light" />
+            </div>
+            <div className="form-container">
+              <Form />
+            </div>
+          </div>
+        </section>
+      </section>
+
+      <section className="bg-dark" />
+    </main>
   </>
 );
 
