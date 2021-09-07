38fb486e7fc812ed31735ae6392ad152bcf13a73
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-06 18:26:29 -0600

    refactor: delete unused imports

--- src/Signup.js
+++ src/Signup.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from './components/button';
 import Header from './components/header';
 import Countdown from './components/countdown';
 import Form from './components/form';
