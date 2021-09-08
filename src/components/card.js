e5b8965b70d657c192dd754c00310007ee8612dc
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-08 11:54:10 -0600

    feat: validate props with prop-types

--- src/components/card.js
+++ src/components/card.js
@@ -1,11 +1,11 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Button from './button';
 
 const Card = (props) => (
-  <div className={`card ${props.cardClassName}`}>
+  <div className={`card ${props.className}`}>
     <div className="card__header">
       <p className="card__tier">{props.tier}</p>
       <div className="card__price">
@@ -28,3 +28,12 @@ const Card = (props) => (
 );
 
 export default Card;
+
+Card.propTypes = {
+  className: PropTypes.string,
+  tier: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  users: PropTypes.string.isRequired,
+  cta: PropTypes.string.isRequired,
+  features: PropTypes.array.isRequired,
+};
