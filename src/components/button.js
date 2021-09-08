a5590d7b39060181850c3dffa798187ed6502897
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-08 11:47:53 -0600

    feat: validate props with prop-types

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,18 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
+import PropTypes from 'prop-types';
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Button = (props) => (
-  <span className={`btn ${props.btnClass}`} type="button">
+  <span className={`btn ${props.className}`} type="button">
     {props.description}
   </span>
 );
 
 export default Button;
+
+Button.propTypes = {
+  className: PropTypes.string,
+  description: PropTypes.string.isRequired,
+};
