fab1a3dcd6f16028d0a89ab57c538f25d680e493
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-15 11:55:31 -0600

    refactor: configure code splitting with React lazy and Suspense

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import App from './App';
-import Signup from './Signup';
+
+const LazyApp = React.lazy(() => import('./App'));
+const LazySignup = React.lazy(() => import('./Signup'));
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-      </Switch>
+      <React.Suspense fallback="Loading...">
+        <Switch>
+          <Route exact path="/" component={LazyApp} />
+          <Route path="/signup" component={LazySignup} />
+        </Switch>
+      </React.Suspense>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
