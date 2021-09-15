15895d76737bd8c1332070d0edfc28901fb3179f
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-09-14 19:17:12 -0600

    feat: I did a lot of things here, please check description
    - change component name from Countdown to CountdownTimer to avoid naming collision with Countdown component
    - use Countdown component to render a timer with 47 days
    - use DateTime to convert the 47 day counter to a regular calendar date: Nov 23, 2021

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,37 +1,55 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Countdown from 'react-countdown';
+import { DateTime } from 'luxon';
 
-const Countdown = (props) => (
-  <>
-    <div className={`countdown ${props.className}`}>
-      <p className="countdown__title">
-        Coming <span>4 Nov 2020</span>
-      </p>
-      <div className="countdown__timer">
-        <div className="time">
-          <h3>47</h3>
-          <p>days</p>
-        </div>
-        <div className="time">
-          <h3>07</h3>
-          <p>hours</p>
-        </div>
-        <div className="time">
-          <h3>56</h3>
-          <p>min</p>
-        </div>
-        <div className="time">
-          <h3>14</h3>
-          <p>secs</p>
-        </div>
-      </div>
-    </div>
-  </>
-);
+const CountdownTimer = (props) => {
+  // This is the format for our DateTime component
+  const format = { year: 'numeric', month: 'short', day: 'numeric' };
+  const fortySevenDaysInMillis = 4060800000;
 
-export default Countdown;
+  return (
+    <>
+      <Countdown
+        date={Date.now() + fortySevenDaysInMillis}
+        renderer={({ days, hours, minutes, seconds }) => (
+          <div className={`countdown ${props.className}`}>
+            <p className="countdown__title">
+              Coming{' '}
+              <span>
+                {DateTime.fromMillis(
+                  Date.now() + fortySevenDaysInMillis
+                ).toLocaleString(format)}
+              </span>
+            </p>
+            <div className="countdown__timer">
+              <div className="time">
+                <h3>{days}</h3>
+                <p>days</p>
+              </div>
+              <div className="time">
+                <h3>{hours}</h3>
+                <p>hours</p>
+              </div>
+              <div className="time">
+                <h3>{minutes}</h3>
+                <p>min</p>
+              </div>
+              <div className="time">
+                <h3>{seconds}</h3>
+                <p>secs</p>
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </>
+  );
+};
 
-Countdown.propTypes = {
+export default CountdownTimer;
+
+CountdownTimer.propTypes = {
   className: PropTypes.string.isRequired,
 };
